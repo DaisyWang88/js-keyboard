3887eae4643013b56034971608185f909a06a240
DaisyWang88 <d1126484584@163.com>
2017-10-30 23:34:25 +0800

    fastclick 添加

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-
+if ('addEventListener' in document) {
+    document.addEventListener('DOMContentLoaded', function(){
+        FastClick.attach(document.body);
+    }, false);
+}
 var inputArea = document.getElementsByClassName('input-area')[0];
 
 // var rightSpace = document.getElementById('right-space');
